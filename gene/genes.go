d9ddfe6795e14f313045b2ec6208bed0a5c35c39
James Reed <jcrd@tuta.io>
2021-01-25 07:07:32 -0700

    Use a map for gene chars

--- gene/genes.go
+++ gene/genes.go
@@ -30,43 +30,27 @@ const (
     N
 )
 
+var geneChars = map[Gene]string{
+    ZERO: "0",
+    FWD: "}",
+    BACK: "{",
+    INC: "+",
+    DEC: "-",
+    READG: "g",
+    WRITEG: "G",
+    READB: "b",
+    WRITEB: "B",
+    LOOP: "[",
+    REP: "]",
+    TURN: "t",
+    XCHG: "x",
+    KILL: "k",
+    SHARE: "s",
+    STOP: ".",
+}
+
 func (g Gene) String() string {
-    switch g {
-    case ZERO:
-        return "0"
-    case FWD:
-        return "}"
-    case BACK:
-        return "{"
-    case INC:
-        return "+"
-    case DEC:
-        return "-"
-    case READG:
-        return "g"
-    case WRITEG:
-        return "G"
-    case READB:
-        return "b"
-    case WRITEB:
-        return "B"
-    case LOOP:
-        return "["
-    case REP:
-        return "]"
-    case TURN:
-        return "t"
-    case XCHG:
-        return "x"
-    case KILL:
-        return "k"
-    case SHARE:
-        return "s"
-    case STOP:
-        return "."
-    default:
-        return ""
-    }
+    return geneChars[g]
 }
 
 func (g Genome) String() string {
